4773b55e9431a8f3475eb104009e1156a876b1ec
Shreshth Arora <sarora_be18@thapar.edu>
2019-07-13 19:17:55 +0530

    Updates

---
+++ precache-manifest.b3ea515807f7a77d03ce020d45b93434.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2564964aa1e50ab9852b60aed9a82c11",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "7d97c0b85497042df218",
+    "url": "/website/static/css/main.664c301b.chunk.css"
+  },
+  {
+    "revision": "118a035fab4192605322",
+    "url": "/website/static/js/2.5966b81f.chunk.js"
+  },
+  {
+    "revision": "7d97c0b85497042df218",
+    "url": "/website/static/js/main.57dfd34f.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "b87f92659e1b862ef0212086e6af7efc",
+    "url": "/website/static/media/121407.b87f9265.jpg"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/website/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/website/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/website/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "2e047091af6fd17360eaff17d671f443",
+    "url": "/website/static/media/earth.2e047091.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/website/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/website/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "1331ba97560cc13103d379a8843ad1dc",
+    "url": "/website/static/media/federico-beccari.1331ba97.jpg"
+  },
+  {
+    "revision": "efe053682ec97d330adad93c55dc7bd0",
+    "url": "/website/static/media/ilya-yakover.efe05368.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/website/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "8a26aabcc3fe4dd8289d9f379c9f48c6",
+    "url": "/website/static/media/joshua-stannard.8a26aabc.jpg"
+  },
+  {
+    "revision": "e06d84cb846db2a463ac3d904ac0c362",
+    "url": "/website/static/media/kaci-baum-2.e06d84cb.jpg"
+  },
+  {
+    "revision": "d65b63163c9caec8e994304bf1b1cfcc",
+    "url": "/website/static/media/landing-page.d65b6316.png"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/website/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/website/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/website/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/website/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/website/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "ed8128994a7eed31e8ebeba831ed040c",
+    "url": "/website/static/media/profile-page.ed812899.png"
+  },
+  {
+    "revision": "29df8117c07250e9b1ea8e3a67627f67",
+    "url": "/website/static/media/soroush-karimi.29df8117.jpg"
+  },
+  {
+    "revision": "fe3a7838dddb1fbf6901af17ca246ea1",
+    "url": "/website/static/media/uriel-soberanes.fe3a7838.jpg"
+  }
+]);
